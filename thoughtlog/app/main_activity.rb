a4d9fb65d8acee9a85509c8ce433861db1253be0
Gian Lazzarini <gianlazzarini@gmail.com>
2016-02-18 01:56:32 -0800

    Deleted un-started RubyMotion android project.

--- thoughtlog/app/main_activity.rb
+++
@@ -1,7 +0,0 @@
-class MainActivity < Android::App::Activity
-  def onCreate(savedInstanceState)
-    super
-
-    setContentView(resources.getIdentifier('main', 'layout', 'com.lazztech.thoughtlogruby'))
-  end
-end
